3dca83cd187c61658dddad34b68e115c4565d1b8
Tixon <tixon24@gmail.com>
2023-05-10 18:32:13 +0300

    reviews (не закончил)

--- wp-content/themes/bato-website/js/modules/sectionAboutReviews.js
+++ wp-content/themes/bato-website/js/modules/sectionAboutReviews.js
@@ -1,72 +1,70 @@
 export default function sectionAboutReviews(el) {
   if (!el) return;
 
-  const mediaHeightMobileExtraSmall = window.innerHeight <= 500;
-  const mediaHeightSmall = window.innerHeight <= 700;
-
   let controller = new ScrollMagic.Controller();
-  const mediaMobile = window.innerWidth <= 767;
-  const mediaLaptop = window.innerWidth <= 1030;
-  const mediaMobileSmall = window.innerWidth <= 350;
+  let timelineSectionAboutReviews = new TimelineMax();
+  let items = el.querySelectorAll(".anim-item");
+  let durationReviews = items.length * 800;
 
-  if (!mediaHeightMobileExtraSmall) {
-    if (!mediaLaptop) {
-      let timelineSectionAboutReviews = new TimelineMax();
-      let items = el.querySelectorAll(".anim-item");
-      let durationReviews = items.length * 800;
+  items.forEach((item, idx) => {
+    let lastItem = items.length - 1 === idx;
 
-      items.forEach((item, idx) => {
-        let lastItem = items.length - 1 === idx;
-
-        if (!lastItem) {
-          timelineSectionAboutReviews
-            .fromTo(
-              [`.anim-item-${idx + 1}`],
-              {},
-              { y: "30px", ease: Linear.easeNone }
-            )
-            .fromTo(
-              [`.anim-item-${idx}`],
-              {},
-              {
-                maxWidth: mediaMobileSmall
-                  ? "280px"
-                  : mediaMobile
-                  ? "80vw"
-                  : "650px",
-                y: "0",
-                ease: Linear.easeNone,
-              },
-              "<"
-            )
-            .fromTo(
-              [`.anim-item-${idx} .go-opacity`],
-              {},
-              { opacity: "0.4", ease: Linear.easeNone },
-              "<"
-            )
-            .fromTo(
-              [`.anim-item-${idx} .reviews-item-bg`],
-              {},
-              { opacity: "0", ease: Linear.easeNone },
-              "<"
-            );
-        }
-      });
-
-      new ScrollMagic.Scene({
-        triggerElement: el,
-        duration: durationReviews,
-        triggerHook: "onLeave",
-        offset: mediaHeightSmall ? -10 : mediaLaptop ? 0 : -20,
-      })
-        .setPin(el)
-        .setTween(timelineSectionAboutReviews)
-        // .addIndicators({name: "section-interactive"})
-        .addTo(controller)
-        .reverse(true);
+    if (!lastItem) {
+      timelineSectionAboutReviews
+        .fromTo(
+          [`.anim-item-${idx + 1}`],
+          {},
+          { y: "30px", ease: Linear.easeNone }
+        )
+        .fromTo(
+          [`.anim-item-${idx}`],
+          {},
+          {
+            maxWidth: "650px",
+            y: "0",
+            ease: Linear.easeNone,
+          },
+          "<"
+        )
+        .fromTo(
+          [`.anim-item-${idx} .go-opacity`],
+          {},
+          { opacity: "0.4", ease: Linear.easeNone },
+          "<"
+        )
+        .fromTo(
+          [`.anim-item-${idx} .reviews-item-bg`],
+          {},
+          { opacity: "0", ease: Linear.easeNone },
+          "<"
+        );
     }
-  }
+  });
 
-  // offset: mediaMobile ? -90 : mediaLaptop ? 70 : -50,
+  new ScrollMagic.Scene({
+    triggerElement: el,
+    duration: durationReviews,
+    triggerHook: "onLeave",
+  })
+    .setPin(el)
+    .setTween(timelineSectionAboutReviews)
+    .addTo(controller)
+    .reverse(true);
+
+    const reviews_slider = [];
+    jQuery(document).find('.reviews-slider').each(function(el) {
+        let _slider = jQuery(this).find('.swiper');
+        let _scrollbar = jQuery(this).find('.slider-scrollbar');
+
+        reviews_slider[el] = new Swiper(_slider[0], {
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            observer: true,
+            scrollbar: {
+                el: _scrollbar[0],
+                draggable: true,
+                hide: false,
+            },
+        });
+    });
 }
